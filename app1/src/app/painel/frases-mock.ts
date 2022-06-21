d54e59debd436af946c24ebe8056b31eb5fb5661
Skasix00 <brunoalexcastelo@gmail.com>
2022-06-21 18:12:03 +0100

    Complete project, future updates are planned

---
+++ app1/src/app/painel/frases-mock.ts
@@ -0,0 +1,8 @@
+import {Frase} from '../shared/frase.model'
+
+export const FRASES: Frase[] = [
+    {fraseEng:'I like to learn', frasePt:'Eu gosto de aprender'},
+    {fraseEng:'I watch tv', frasePt: 'Eu vejo tv'},
+    {fraseEng:'How are you?', frasePt: 'Como estás'},
+    {fraseEng:'I eat bread', frasePt: 'Eu como pão'},
+]
